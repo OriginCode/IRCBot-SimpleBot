0e180b2c16ad89b36fea5e98138db21b4bcc1f59
OriginCode <wky031123@126.com>
2017-07-24 08:50:39 +0800

    Added multiple channel support.

--- SimpleBot.py
+++ SimpleBot.py
@@ -8,7 +8,7 @@ import re
 # Global Information
 NETWORK = 'irc.freenode.net'
 NICK = 'SimpleBot'
-CHAN = 'linuxba'
+CHAN = ['archlinux-cn-offtopic', 'linuxba']
 PORT = 6697
 PASSWD = 'Aa32504863'
 
@@ -24,7 +24,8 @@ irc = ssl.wrap_socket(socket)
 irc.send('PASS %s\r' % PASSWD)
 irc.send('NICK %s\r' % NICK)
 irc.send('USER %s %s %s :SimpleBot\r' % (NICK, NICK, NICK))
-irc.send('JOIN #%s\r' % CHAN)
+irc.send('JOIN #%s\r' % CHAN[0])
+irc.send('JOIN #%s\r' % CHAN[1])
 
 # Calculate Author:niunai
 l1_pattern = re.compile(r'\([^()]*\)')
@@ -85,6 +86,7 @@ while True:
     data = irc.recv(4096)
     print data
     user = data[data.find(':') + 1:data.find('!')]
+    chan = data[data.find('PRIVMSG') + 9:data.find('::') - 1]
 
     if data.find('PING') != -1:
         irc.send('PONG ' + data.split()[1] + '\r')
@@ -92,10 +94,10 @@ while True:
     if data.find('::') != -1:
         inc = data[data.find('::') + 2:len(data) - 1]
         if re.match(r'^test\r$', inc):
-            irc.send('PRIVMSG #%s :Success!\r' % CHAN)
+            irc.send('PRIVMSG #%s :Success!\r' % chan)
 
         elif re.match(r'^help\r$', inc):
-            irc.send('PRIVMSG #%s :%s: See the private chat.\r' % (CHAN, user))
+            irc.send('PRIVMSG #%s :%s: See the private chat.\r' % (chan, user))
             irc.send('PRIVMSG %s :The command of %s starts with \":\".\r' % (user, NICK))
             irc.send('PRIVMSG %s :----------Help of %s----------\r' % (user, NICK))
             irc.send('PRIVMSG %s :[version]Show the version of %s.\r' % (user, NICK))
@@ -105,32 +107,32 @@ while True:
             irc.send('PRIVMSG %s :[calc ...]Calculator.\r' % user)
 
         elif re.match(r'^version\r$', inc):
-            irc.send('PRIVMSG #%s :%s: 3.1\r' % (CHAN, user))
+            irc.send('PRIVMSG #%s :%s: 3.1\r' % (chan, user))
 
         elif re.match(r'^fortune\r$', inc):
             output = os.popen('fortune').read().split('\n')
             for i in xrange(len(output) - 1):
-                irc.send('PRIVMSG #%s :%s\r' % (CHAN, output[i].replace('\t', '    ')))
+                irc.send('PRIVMSG #%s :%s\r' % (chan, output[i].replace('\t', '    ')))
 
         elif re.match(r'^echo\s\w*\r$', inc):
-            irc.send('PRIVMSG #%s :%s\r' % (CHAN, inc[inc.find('echo') + 5:len(inc) - 1]))
+            irc.send('PRIVMSG #%s :%s\r' % (chan, inc[inc.find('echo') + 5:len(inc) - 1]))
 
         # Time
 
         elif re.match(r'^time\r$', inc):
             os.environ['TZ'] = 'Asia/Shanghai'
             time.tzset()
-            irc.send('PRIVMSG #%s :%s: Time: %s (CST/GMT+8)\r' % (CHAN, user, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())))
+            irc.send('PRIVMSG #%s :%s: Time: %s (CST/GMT+8)\r' % (chan, user, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())))
 
         elif re.match(r'^time\stz:\d{1,3}\r$', inc):
             if -12 <= int(inc[inc.find('tz:') + 3:len(inc) - 1]) <= 14:
-                irc.send('PRIVMSG #%s :%s: Time: %s (CST/GMT+%s)\r' % (CHAN, user, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time() - 8 * 3600 + int('%s' % inc[inc.find('tz:') + 3:len(inc) - 1]) * 3600)), int(inc[inc.find('tz:') + 3:len(inc) - 1])))
+                irc.send('PRIVMSG #%s :%s: Time: %s (CST/GMT+%s)\r' % (chan, user, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time() - 8 * 3600 + int('%s' % inc[inc.find('tz:') + 3:len(inc) - 1]) * 3600)), int(inc[inc.find('tz:') + 3:len(inc) - 1])))
 
             else:
-                irc.send('PRIVMSG #%s :%s: Argument must be lower than 14 and higher than -12\r' % (CHAN, user))
+                irc.send('PRIVMSG #%s :%s: Argument must be lower than 14 and higher than -12\r' % (chan, user))
 
         elif re.match(r'^time\suts\r$', inc):
-            irc.send('PRIVMSG #%s :%s: Unix Timestamp: %s\r' % (CHAN, user, time.time()))
+            irc.send('PRIVMSG #%s :%s: Unix Timestamp: %s\r' % (chan, user, time.time()))
 
         # Calculate
 
@@ -141,13 +143,13 @@ while True:
                 l1_analysis(s)
 
             except AttributeError, errout:
-                irc.send('PRIVMSG #%s :%s: %s\r' % (CHAN, user, errout))
+                irc.send('PRIVMSG #%s :%s: %s\r' % (chan, user, errout))
                 continue
 
-            irc.send('PRIVMSG #%s :%s: %s\r' % (CHAN, user, l1_analysis(s)))
+            irc.send('PRIVMSG #%s :%s: %s\r' % (chan, user, l1_analysis(s)))
 
         elif user == 'OriginCode':
             if re.match(r'^sh\s.*\r$', inc):
                 output = os.popen(inc[inc.find('sh') + 3:len(inc) - 1]).read().split('\n')
                 for i in xrange(len(output) - 1):
-                    irc.send('PRIVMSG #%s :%s\r' % (CHAN, output[i].replace('\t', '    ')))
+                    irc.send('PRIVMSG #%s :%s\r' % (chan, output[i].replace('\t', '    ')))
