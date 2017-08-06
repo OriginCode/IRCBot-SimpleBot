74675c4a72932aa139c59c385a724154f56a1e4b
OriginCode <wky031123@126.com>
2017-08-06 22:47:03 +0800

    ???

--- module/base.py
+++ module/base.py
@@ -4,7 +4,4 @@ CHAN = ['linuxba', 'archlinux-cn', 'tox-cn']
 PORT = 6697
 PASSWD = 'PASSWORD'
 ADMIN_PASSWD = 'ADMIN PASSWORD'
-<<<<<<< HEAD
 
-=======
->>>>>>> b4034c2219840fd9709ae5b94bdee651bae4f7e1
