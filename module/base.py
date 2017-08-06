4937fab191603556ad41ada677e29747052e6c26
OriginCode <wky031123@126.com>
2017-08-06 22:44:30 +0800

    ...

--- module/base.py
+++ module/base.py
@@ -2,5 +2,6 @@ NETWORK = 'irc.freenode.net'
 NICK = 'SimpleBot'
 CHAN = ['linuxba', 'archlinux-cn', 'tox-cn']
 PORT = 6697
-PASSWD = 'Aa32504863'
-ADMIN_PASSWD = '23333'
+PASSWD = 'PASSWORD'
+ADMIN_PASSWD = 'ADMIN PASSWORD'
+
