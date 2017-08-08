41e8f38e1769fa655430584717c30c23155f227e
OriginCode <wky031123@126.com>
2017-08-08 14:41:00 +0800

    Remove new editing.

--- module/base.py
+++ module/base.py
@@ -1,6 +1,6 @@
 NETWORK = 'irc.freenode.net'
 NICK = 'SimpleBot'
-CHAN = ['liuyanbot', 'botest', 'berton-research']
+CHAN = ['linuxba', 'archlinux-cn', 'tox-cn']
 PORT = 6697
 PASSWD = 'PASSWORD'
 ADMIN_PASSWD = 'ADMIN PASSWORD'
