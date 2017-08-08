0249f25d0101fd710a49404c8e8cb541fbe47381
OriginCode <wky031123@126.com>
2017-08-08 14:37:55 +0800

    TESTING

--- module/base.py
+++ module/base.py
@@ -1,6 +1,6 @@
 NETWORK = 'irc.freenode.net'
 NICK = 'SimpleBot'
-CHAN = ['linuxba', 'archlinux-cn', 'tox-cn']
+CHAN = ['liuyanbot', 'botest', 'berton-research']
 PORT = 6697
 PASSWD = 'PASSWORD'
 ADMIN_PASSWD = 'ADMIN PASSWORD'
