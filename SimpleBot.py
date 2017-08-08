a1a6b60be559deee502780a7bb0d4330325aa069
OriginCode <wky031123@126.com>
2017-08-08 15:02:31 +0800

    Added a user search function. (GitHub)

--- SimpleBot.py
+++ SimpleBot.py
@@ -114,6 +114,23 @@ def main():
                     insert = inc[inc.find('github(all)') + 12:len(inc) - 1].replace(' ', '+')
                     irc.send('PRIVMSG %s :%s: --> https://github.com/search?q=%s <--\r' % (chan, user, insert))
 
+                elif re.match(r'^github\(user\)\s.+\r$', inc):
+                    insert = inc[inc.find('github(user)') + 13:len(inc) - 1].replace(' ', '+')
+                    print insert
+                    curl = os.popen('curl -H \'Accept: application/vnd.github.v3.text-match+json\' https://api.github.com/search/users\?q\=%s' % insert).read()
+                    try:
+                        name = re.findall(r'\"login\":.+', curl)[0].split(': ')[1]
+                        connection = re.findall(r'\"html_url\":.+', curl)[0].split(': ')[1]
+
+                    except IndexError, errout:
+                        irc.send('PRIVMSG %s :%s: tan 90°\r' % (chan, user))
+                        print errout
+                        continue
+
+                    name_ = name[name.find('\"') + 1:len(name) - 2]
+                    connection_ = connection[connection.find('\"') + 1:len(connection) - 2]
+                    irc.send('PRIVMSG %s :%s: Top: [ %s ] - %s\r' % (chan, user, name_, connection_))
+
                 elif re.match(r'^github\s.+\r$', inc):
                     insert = inc[inc.find('github') + 7:len(inc) - 1].replace(' ', '+')
                     curl = os.popen('curl -H \"Authentication: token TOKEN\" -H \"Accept: application/vnd.github.mercy-preview+json\" https://api.github.com/search/repositories\?q\=%s' % insert).read()
