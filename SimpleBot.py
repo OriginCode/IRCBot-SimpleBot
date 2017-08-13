86d3444c5c7a49eb5a8b0ce7ae92fcbc4ea258ec
OriginCode <wky031123@126.com>
2017-08-13 22:28:47 +0800

    Optimized the output.

--- SimpleBot.py
+++ SimpleBot.py
@@ -197,7 +197,7 @@ def main():
                         print errout
                         continue
 
-                    irc.send('PRIVMSG %s :%s: [ %s - %s ] Weather: %s Current Temperature: %d Wind Speed: %s\r' % (chan, user, country, city, weather, temp, wind_speed))
+                    irc.send('PRIVMSG %s :%s: [ %s - %s ] Weather: %s, Current Temperature: %d°C, Wind Speed: %s.\r' % (chan, user, country, city, weather, temp, wind_speed))
 
                 elif re.match(r'^tell\s#.+\s.+\r$', inc):
                     regex_split = re.split('\s', inc)
